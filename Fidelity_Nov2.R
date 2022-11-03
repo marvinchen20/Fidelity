093eea7c5bdcd16d9534e967f140b0eafae94649
Marvin <marvinchen2020@gmail.com>
2022-11-02 22:33:56 -0400

    Add files via upload

---
+++ Fidelity_Nov2.R
@@ -0,0 +1,54 @@
+library(tidyverse)
+library(tidytext)
+library(janeaustenr)
+library(stringr)
+library(ggplot2)
+IMDB.DataSet <- read.csv("D:/BU/Fidelity/IMDB Dataset.csv")
+IMDB <- tibble(IMDB.DataSet)
+
+#check the if words is uppercase
+upcheck <- function(x){
+  y <- toupper(x)
+  x == y
+}
+
+#unnest the review to words
+book_words <- IMDB %>% 
+  unnest_tokens(word, review,to_lower = F)
+
+#filter all Uppercase words
+tidy_test <- book_words %>%filter(upcheck(book_words$word) == T)
+tidy_test <- tidy_test %>% 
+  unnest_tokens(word,word)
+
+
+#attatch stop_words dataset
+data(stop_words)
+stop_word <-select(stop_words,word)
+stop_word1 <- c(1:1000)
+stop_word1 <- as.data.frame(stop_word1) %>% rename(word = stop_word1)
+stop_word2 <- c("tv","dvd","2001")
+stop_word2 <- as.data.frame(stop_word2) %>% rename(word = stop_word2)
+
+stop_word <- rbind.data.frame(stop_word,stop_word1,stop_word2)
+
+
+
+#anti join the stop words
+tidy_books <- tidy_test %>%
+  anti_join(stop_word)
+
+test_tib <- tidy_books %>% count(word, sort = TRUE) 
+
+
+#grapsh
+tidy_books %>%
+  count(word, sort = TRUE) %>%
+  filter(n > 200) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(n, word)) +
+  geom_col() +
+  labs(y = NULL)
+
+
+
