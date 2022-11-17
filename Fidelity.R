e2ad3595ce8f5e60ae33ba666dc0dd4a8db9c2d0
Marvin <marvinchen2020@gmail.com>
2022-11-17 04:48:42 -0500

    Add files via upload

--- Fidelity.R
+++ Fidelity.R
@@ -11,13 +11,14 @@ IMDB <- tibble(IMDB.Dataset)
 IMDB <- IMDB  %>%  mutate(docs = c(1:length(IMDB$review)))
 
 data(stop_words)
-stop_words <- rbind(stop_words,c("br","Smart"))
+my_stop_word <- c("br","Smart","film","Smart","movie","Smart","time","Smart")
+stop_words <- rbind(stop_words,my_stop_word)
 
 
 
 
 
-###  LDA
+###cluster
 imdb_dtm <- IMDB %>%
   unnest_tokens(word, review) %>%
   anti_join(stop_words)%>%
@@ -25,9 +26,43 @@ imdb_dtm <- IMDB %>%
   cast_dtm(docs, word, n)
 
 
+
+
+
+
+
+###  LDA
 ap_lda <- LDA(imdb_dtm, k = 10, control = list(seed = 1234))
 
 ap_topics <- tidy(ap_lda, matrix = "beta")
+
+perplexity(ap_lda)
+
+n_topics <- c(10:20)
+
+
+
+ap_lda_compare <- n_topics %>% 
+  map(LDA, x = imdb_dtm, control = list(seed = 1109)) 
+ 
+perplexity(ap_lda_compare)
+
+plot(n_topics,ap_lda_compare)
+
+
+
+ 
+
+
+
+
+
+ggplot(ap_lda_compare , aes(x = k, y = perplexity)) +
+  geom_point() +
+  geom_smooth(se = FALSE) +
+  ggtitle("5-fold cross-validation of topic modelling with the 'Associated Press' dataset",
+          "(ie five different models fit for each candidate number of topics)") +
+  labs(x = "Candidate number of topics", y = "Perplexity when fitting the trained model to the hold-out set")
 
 ap_top_terms <- ap_topics %>%
   group_by(topic) %>%
@@ -41,6 +76,8 @@ ap_top_terms %>%
   geom_col(show.legend = FALSE) +
   facet_wrap(~ topic, scales = "free") +
   scale_y_reordered()
+
+perplexity(ap_lda)
 ###
 
 
